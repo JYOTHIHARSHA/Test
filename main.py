37f6c775f24ac9218e600759d33dcab64a4ec30e
Your Name <you@example.com>
2022-09-15 11:47:38 +0530

    'initial'

---
+++ main.py
@@ -0,0 +1,18 @@
+l=[]
+for i in range(2000, 3201):
+    if (i%7==0) and (i%5!=0):
+        l.append(str(i))
+print(','.join(l))
+
+# string length
+string = "jyothi"
+print(len(string))
+
+# string into integer and floate
+string ="2"
+print(int(string))
+
+print(float(string))
+
+
+
